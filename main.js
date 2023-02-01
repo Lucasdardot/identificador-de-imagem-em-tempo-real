19e908b2ce4b164c62073b0ad8c5fa9d0d7670c7
Lucasdardot <105593880+Lucasdardot@users.noreply.github.com>
2023-02-01 13:27:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function setup() {
+  canvas = createCanvas(350, 350);
+  video = createCapture(VIDEO);
+  
+  calssifier = ml5.imageClassifier('MobileNet',modelLoaded)
+}
+
+function modelLoaded(){
+  console.log("MobileNet foi carregado!")
+}
+
+function draw(){
+  image(video,0,0, 350,350)
+  cassifier.classify(video, gotResult)
+}
+
+var resultadoanterior = ""
+
+function gotResult(error, results){
+  if (error){
+    console.log(error)
+  }
+  else{
+    if((results[0].confidence > 0.5)&&(resultadoanterior != results[0].label)){
+      console.log(results)
+      resultadoanterior = results[0].label
+      var synth = window.speechSynthesis
+      speakData = "O objeto detectado é " +results[0].label
+      var Faleisso = new SpeechSynthesisUtterance(speakData)
+      synth.speak(Faleisso)
+      document.getElementById("resultObjectName").innerHTML = results[0].label
+      document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(2)
+
+    }
+  }
+}
+
